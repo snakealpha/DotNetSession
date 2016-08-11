36d603e8004691f9ef5984ee3750a10ed89ff30d
Snake Liu <yiyiyouxia@live.cn>
2016-08-11 22:24:55 +0800

    Initialize repo.

---
+++ SampleProject/Sample Project/Assets/Scripts/SampleClasses.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    abstract class SampleBase
+    {
+        public static string ClassFieldSample = "Miaow!";
+        public int InstanceFieldSample;
+    }
+
+    class SampleSubclass : SampleBase
+    {
+        public int SampleProperty { get; set; }
+        
+        public string SampleMethod()
+        {
+            List<string> baseList = new List<string>();
+            for (int i = 0; i < 255; i++)
+            {
+                baseList.Add(i.ToString());
+            }
+
+            int randomResult = new System.Random().Next()%256;
+            foreach (var item in baseList)
+            {
+                if (randomResult.ToString() == item)
+                    return item;
+            }
+
+            return "";
+        }
+    }
+}
